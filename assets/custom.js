bed3733819b89213f6cfdcf628e85abefa30f8c2
rAlabasterPlanning <mastermatrasbv@gmail.com>
2026-09-10 11:16:35 +0200

    Voeg uitbreidingslaag toe

---
+++ assets/custom.js
@@ -0,0 +1,3 @@
+// Veilige uitbreidingslaag voor toekomstige kleine aanpassingen.
+// Plaats hier losse UI- of bedrijfsregels zonder assets/app.js te hoeven herschrijven.
+window.RALAB_HOOKS = window.RALAB_HOOKS || {};
